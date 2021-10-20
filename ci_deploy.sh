cfb5f10d0898e17381df11e8840da08497506722
Neth Maison <monumentcalifornia@gmail.com>
2021-10-19 19:15:42 -0700

    added ami

---
+++ ci_deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+image=node_deployer
+docker build . -t $image -f Dockerfile.deploy 
+app_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+docker run --rm \
+  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+  -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+  -e ENVIRONMENT=$ENVIRONMENT \
+  $image /bin/sh -c '/bin/sh -c "lerna run deploy"'
+
