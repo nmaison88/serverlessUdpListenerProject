0bd09a5cabc66980b2f68e21e23b78fa1ef61b8b
Neth Maison <monumentcalifornia@gmail.com>
2021-10-27 13:51:42 -0700

    rmoved fifo and added iam instance role

--- ec2Start.sh
+++ ec2Start.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 yum update -y
-sudo yum install docker
+sudo amazon-linux-extras install docker
 service docker start
 usermod -a -G docker ec2-user
 chkconfig docker on
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 686363414642.dkr.ecr.us-west-2.amazonaws.com
 docker pull 686363414642.dkr.ecr.us-west-2.amazonaws.com/udp/listener:latest
-docker run -d --network=host -e 686363414642.dkr.ecr.us-west-2.amazonaws.com/udp/listener:latest
+docker run -e QUEUE_URL='https://sqs.us-west-2.amazonaws.com/686363414642/alpha-listener-sqs' -d --network=host 686363414642.dkr.ecr.us-west-2.amazonaws.com/udp/listener:latest
